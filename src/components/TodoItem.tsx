a9feddfea263d5eb081138b411eb35c3f8caeab8
Lucas Cristiano Passini <47937044+lucaspassini@users.noreply.github.com>
2022-10-12 21:30:44 -0300

    add: css modules & remove handler

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,15 @@
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li> {props.text}</li>;
+import classes from './TodoItem.module.css';
+
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: () => void;
+}> = (props) => {
+  return (
+    <li className={classes.items} onClick={props.onRemoveTodo}>
+      {' '}
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
